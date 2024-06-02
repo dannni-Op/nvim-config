df3e4414e21de6eac9570faf7b2925db2a8e89e3
tanpa-nama <tanpa-nama>
2024-06-02 17:28:03 +0700

    docs: setup git

---
+++ lua/config/lazy.lua
@@ -0,0 +1,142 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Example using a list of specs with the default options
+vim.g.mapleader = "" -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.maplocalleader = "" -- Same for `maplocalleader`
+
+require("lazy").setup({
+    "folke/which-key.nvim",
+    { "folke/neoconf.nvim", cmd = "Neoconf" },
+    "folke/neodev.nvim",
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {
+            style = "moon"
+        },
+    },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        end,
+    },
+    {
+        'numToStr/Comment.nvim',
+        opts = {
+            -- add any options here
+        },
+        lazy = false,
+    },
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+    {
+        "hrsh7th/nvim-cmp",
+        version = false, -- last release is way too old
+        event = "InsertEnter",
+        dependencies = {
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/nvim-cmp',
+        },
+        opts = function()
+            local cmp = require('cmp')
+            local defaults = require("cmp.config.default")()
+            return {
+                auto_brackets = {}, -- configure any filetype to auto add brackets
+                completion = {
+                    completeopt = "menu,menuone,noinsert",
+                },
+                mapping = cmp.mapping.preset.insert({
+                    -- ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+                    -- ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<CR>'] = cmp.mapping.confirm({ 
+                        -- behavior = cmp.ConfirmBehavior.Replace,
+                        select = true 
+                    }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                }),
+                sources = cmp.config.sources({
+                    { name = 'path' },
+                }, {
+                    { name = 'buffer' },
+                })
+            }
+        end
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        opts = function()
+            return {
+                options = {
+                    icons_enabled = false,
+                    theme = 'horizon',
+                    component_separators = "",
+                    section_separators = "",
+                    globalstatus = true,
+                    refresh = {
+                        statusline = 1000,
+                        tabline = 1000,
+                        winbar = 1000,
+                    }
+                },
+                sections = {
+                    lualine_a = {'mode'},
+                    lualine_b = {'branch', 'diff', 'diagnostics'},
+                    lualine_c = {'filename'},
+                    lualine_x = {'encoding', 'fileformat', 'filetype'},
+                    lualine_y = {'progress'},
+                    lualine_z = {'location'}
+                },
+            }
+        end
+    },
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true
+        -- use opts = {} for passing setup options
+        -- this is equalent to setup({}) function
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function () 
+            local configs = require("nvim-treesitter.configs")
+
+            configs.setup({
+                    -- ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "php" },
+                    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", 'css', 'json', "php" },
+                    sync_install = false,
+                    highlight = { enable = true },
+                    indent = { enable = true },  
+                    })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function ()
+            local lspconfig = require('lspconfig')
+            -- lspconfig.lua_ls.setup({})
+        end
+    },
+})
